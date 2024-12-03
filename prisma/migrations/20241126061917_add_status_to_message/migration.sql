c36303ca6ddb2653b131907d8b5acfa2230117d3
Erik Paluka <erik.paluka@gmail.com>
2024-12-03 16:01:58 +0700

    chore(prisma): remove migration files

--- prisma/migrations/20241126061917_add_status_to_message/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "MessageStatus" AS ENUM ('sent', 'delivered', 'read', 'failed');
-
--- AlterTable
-ALTER TABLE "Message" ADD COLUMN     "status" "MessageStatus" NOT NULL DEFAULT 'sent';
